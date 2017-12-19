e5d7196a4c721c41cceccf730c19e961446016cc
DimGael <gael.dim@etu.unilim.fr>
2017-12-19 15:16:46 +0100

    Dernier commit

--- src/main/java/m3105/kebabvisitor/Kebab.java
+++ src/main/java/m3105/kebabvisitor/Kebab.java
@@ -7,18 +7,19 @@ import m3105.kebabvisitor.visitor.VisiteurDeRegime;
 import m3105.kebabvisitor.visitor.VisiteurDeRegimeVegetarien;
 
 public abstract class Kebab {
+
 	public abstract List<Ingredient> getIngredients();
-	
+
+	public abstract boolean isPescetarien();
+
 	public boolean isVegetarien() {
 		VisiteurDeRegime visiteur = new VisiteurDeRegimeVegetarien();
-		
-		for(Ingredient ingredient : this.getIngredients()) {
-			if(!ingredient.accept(visiteur))
+
+		for (Ingredient ingredient : this.getIngredients()) {
+			if (!ingredient.accept(visiteur))
 				return false;
 		}
-		
+
 		return true;
 	}
-	
-	public abstract boolean isPescetarien();
 }
---
+++ src/main/java/m3105/kebabvisitor/visitor/VisiteurDeRegimePescétarien.java
@@ -0,0 +1,77 @@
+package m3105.kebabvisitor.visitor;
+
+import m3105.kebabvisitor.ingredients.Agneau;
+import m3105.kebabvisitor.ingredients.Boeuf;
+import m3105.kebabvisitor.ingredients.Crevette;
+import m3105.kebabvisitor.ingredients.Fromage;
+import m3105.kebabvisitor.ingredients.Oignon;
+import m3105.kebabvisitor.ingredients.Pain;
+import m3105.kebabvisitor.ingredients.Salade;
+import m3105.kebabvisitor.ingredients.Sauce;
+import m3105.kebabvisitor.ingredients.Thon;
+import m3105.kebabvisitor.ingredients.Tomate;
+
+public class VisiteurDeRegimePescétarien implements VisiteurDeRegime {
+
+	@Override
+	public boolean visit(Agneau agneau) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean visit(Boeuf element) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean visit(Crevette element) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean visit(Fromage element) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean visit(Oignon element) {
+		return false;
+	}
+
+	@Override
+	public boolean visit(Pain element) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean visit(Salade element) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean visit(Sauce element) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean visit(Thon element) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean visit(Tomate element) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
+	
+}
